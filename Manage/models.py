e6551a10b59c5af1deaac619ef7a9b229fc1e1ec
Erdem Star <erdem-yildiz@windowslive.com>
2018-04-06 17:10:06 +0300

    Words tablosu oluşturuldu

--- Manage/models.py
+++ Manage/models.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Words(models.Model):
+    englishWord     = models.CharField(max_length=100 , blank=False) # English word  i don't know
+    turkishWord    = models.CharField(max_length=200 , blank=False) # Write meaning in turkish
+    type            = models.CharField(max_length=100 , blank=False) # Adjective , verb , etc.
+    sentence        = models.CharField(max_length=1000 , blank=False) # create one or more sentence
+
