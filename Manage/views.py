1bae82bb27b243dfa09d7114c172aed7a2562136
Erdem Star <erdem-yildiz@windowslive.com>
2018-04-07 13:57:01 +0300

    Kelime sorma sayfası üzerinde birkaç güncelleme yapıldı.Views kısmıda ise KelimeSorma ile alakaı uygun bir algoritma yazıldı.

--- Manage/views.py
+++ Manage/views.py
@@ -3,8 +3,12 @@ from __future__ import unicode_literals
 from django.http import *
 from django.shortcuts import render
 from models import Words
+import random
 
-# Create your views here.
+#-------------------
+allWord = []
+
+#-------------------
 
 
 def Yonlendir(request):
@@ -43,4 +47,31 @@ def KelimeListele(request):
 
 
 def KelimeSorma(request):
-    return render(request,"KelimeSorma.html",{})
+    if request.method == "GET":
+        global allWord
+        getir = Words.objects.all()
+        for i in getir:
+            allWord.append(i)
+        random.shuffle(allWord)
+        return render(request,"KelimeSorma.html",{"arr":allWord[:5]})
+    elif request.method == "POST":
+        global allWord
+        score = 0
+        t1 = request.POST["t1"].encode('utf-8')
+        t2 = request.POST["t2"].encode("utf-8")
+        t3 = request.POST["t3"].encode("utf-8")
+        t4 = request.POST["t4"].encode("utf-8")
+        t5 = request.POST["t5"].encode("utf-8")
+        for i in allWord[:5]:
+            if t1 in i.turkishWord.encode('utf-8'):
+                score+=20
+            elif t2 in i.turkishWord.encode('utf-8'):
+                score+=20
+            elif t3 in i.turkishWord.encode('utf-8'):
+                score += 20
+            elif t4 in i.turkishWord.encode('utf-8'):
+                score += 20
+            elif t5 in i.turkishWord.encode('utf-8'):
+                score += 20
+
+        return render(request, "KelimeSorma.html", {"score": score,"arr":allWord[:5]})
